ca08ce2f0200d515a9178701e2057571f9e40b86
Ross <23426097+Rlbregant@users.noreply.github.com>
2022-12-12 14:51:08 -0700

    Update WriteCSVFile.java

--- src/com/coderscampus/WriteCSVFile.java
+++ src/com/coderscampus/WriteCSVFile.java
@@ -3,7 +3,6 @@ package com.coderscampus;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.List;
 
 public class WriteCSVFile {
