e07e773ecb805abeec3e3abf56e32af8d29de166
Ross <23426097+Rlbregant@users.noreply.github.com>
2022-12-20 09:51:01 -0700

    Final

--- src/com/coderscampus/ReadDataFile.java
+++ src/com/coderscampus/ReadDataFile.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadDataFile {
+
 	public List<String[]> readDataFile(String filename) {
 		List<String[]> data = new ArrayList<>();
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
--- src/com/coderscampus/WriteCSVFile.java
+++ src/com/coderscampus/WriteCSVFile.java
@@ -9,15 +9,15 @@ public class WriteCSVFile {
 	public void writeCSVFile(List<String[]> data, String filename) {
 	    try (PrintWriter pw = new PrintWriter(new File(filename))) {
 	        // Print the column names
-	        pw.println("Student ID    Student Name  	      Course  	Grade");
+	        pw.println("Student ID,    Student Name,  	      Course,  	Grade");
 
 	        // Print a line of dashes to separate the column names from the data
-	        pw.println("------------  ------------  	      ------  	-----");
+	        pw.println("------------,  ------------,  	      ------------,  	---------------");
 
 	        // Iterate over the elements of the data list
 	        for (String[] student : data) {
 	            // Print the elements of the record array with left alignment
-	            pw.printf("%-12s  %-20s  %-12s  %-12s%n", student[0], student[1], student[2], student[3]);
+	            pw.println(student[0] + ", " + student[1] + ", " + student[2] + ", " + student[3]);
 	        }
 	    } catch (IOException e) {
 	        e.printStackTrace();
